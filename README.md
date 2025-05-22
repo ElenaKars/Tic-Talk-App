3937c6d65958cf802d96fb6ac80a3082ab6403d0
ElenaKars <elena_baldina@yahoo.com>
2025-05-22 14:21:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Tic-Talk
+
+**Tic-Talk** is a modular web application built with Angular, using modern features like standalone components, a service-based architecture, and SCSS for styling. The app emphasizes clean structure, reusability, and scalability.
+
+## Features
+
+- Angular standalone components (no traditional NgModules)
+- Reusable UI components (e.g., `ProfileCardComponent`)
+- Service-driven data handling via `ProfileService`
+- Custom styles with SCSS
+- Static assets (e.g., fonts) loaded from the `public/assets` directory
+
+## Tech Stack
+
+- Angular 16+
+- TypeScript
+- RxJS
+- SCSS
+
+
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfileService } from './data/services/profile.service';
+import { CommonModule } from '@angular/common';
+import { Profile } from './data/interfaces/profile.interface';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, ProfileCardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
+}
